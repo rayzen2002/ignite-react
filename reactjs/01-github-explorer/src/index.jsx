5feb360ccb6de3f44e5ba844e55eb9a89e3d7b6f
rayzen2002 <emanuelzsmj@gmail.com>
2021-03-08 15:45:05 -0300

    Utiliziando SASS

--- reactjs/01-github-explorer/src/index.jsx
+++ reactjs/01-github-explorer/src/index.jsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom'
 import { App } from './app'
-import { global } from '../styles/global.css'
+import { global } from '../styles/global.scss'
 
 render(<App /> , document.getElementById('root'))
 
