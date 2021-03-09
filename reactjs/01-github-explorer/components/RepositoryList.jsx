0e26a617c2980d9bce0c3c717c770d3e137447cf
rayzen2002 <emanuelzsmj@gmail.com>
2021-03-09 15:28:26 -0300

    Estilizacao da listagem e Utilizando o useEffect

--- reactjs/01-github-explorer/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+import { useState, useEffect } from "react";
+
 const repository = {
     name: 'unform',
     description: 'Forms in React',
@@ -6,6 +9,16 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories , setRepositories] = useState([]);
+    
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json())
+        .then(data => {
+            setRepositories(data)
+        })
+
+    } , [repositories])
+    
     return(
         <section className="repository-list">
             <h1 className="">Lista de Repositorios</h1>
--- reactjs/01-github-explorer/src/app.jsx
+++ reactjs/01-github-explorer/src/app.jsx
@@ -1,12 +1,11 @@
 import { RepositoryList } from "../components/RepositoryList";
-import { Counter } from "../components/Counter";
+
 
 export function App(){
     
     return(
         <>
             <RepositoryList />
-            <Counter />
         </>
     );
     
