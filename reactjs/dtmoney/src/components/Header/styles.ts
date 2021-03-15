88355d48e6338077153839e91504d203d9459ea0
rayzen2002 <emanuelzsmj@gmail.com>
2021-03-15 14:09:36 -0300

    Corrigindo estrutura de pastas

--- reactjs/dtmoney/src/components/Header/styles.ts
+++ reactjs/dtmoney/src/components/Header/styles.ts
@@ -7,12 +7,10 @@ export const Container = styled.header`
 export const Content = styled.div `
     max-width: 1120px;
     margin: 0 auto;
-
     padding: 2rem 1rem 12rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
-
     button{
         font-size: 1rem;
         color: #FFF;
@@ -20,13 +18,10 @@ export const Content = styled.div `
         padding: 0 2rem;
         border-radius: 0.25rem;
         height: 3rem;
-
         transition: filter 0.2s;
-
         &:hover {
         filter: brightness(0.9);
     }
     }
-
     
 `
