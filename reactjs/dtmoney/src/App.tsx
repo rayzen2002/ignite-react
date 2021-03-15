b7e264386b5197b1d69c38a9549d07eaa1be5192
rayzen2002 <emanuelzsmj@gmail.com>
2021-03-15 16:57:36 -0300

    Componente: TransactionTable

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { TransactionTable } from './components/TransactionTable';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
@@ -8,6 +8,7 @@ export function App() {
     <>
       <Header />
       <Dashboard />
+      <TransactionTable />
       <GlobalStyle />
     </>
   );
